45b38d70fd1521a7b795e7d4aedc57fd4f0b251b
redsheep <codingsouls@gmail.com>
2016-07-14 22:24:58 +0800

    update page

---
+++ javascripts/PreloadRSGUI.js
@@ -0,0 +1,23 @@
+var path=[];
+path.push("../../RSGUI/GUIObject.js");
+path.push("../../RSGUI/GUIContainer.js");
+
+path.push("../../RSGUI/components/button.js");
+path.push("../../RSGUI/components/checkbox.js");
+path.push("../../RSGUI/components/radiobox.js");
+path.push("../../RSGUI/components/dropdown.js");
+path.push("../../RSGUI/components/listview.js");
+path.push("../../RSGUI/components/processbar.js");
+path.push("../../RSGUI/components/rating.js");
+path.push("../../RSGUI/components/roundbutton.js");
+path.push("../../RSGUI/components/scrollbar.js");
+path.push("../../RSGUI/components/textinput.js");
+path.push("../../RSGUI/components/tooltip.js");
+path.push("../../RSGUI/components/toggle.js");
+path.push("../../RSGUI/components/window.js");
+path.push("../../RSGUI/components/label.js");
+
+path.push("../../RSGUI/GUIFactory.js");
+path.push("../../RSGUI/RSGUI.js");
+
+head.load(path);
