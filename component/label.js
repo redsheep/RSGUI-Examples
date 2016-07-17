c4057bef1c5e2ae3babb72b4f3c4283177c81d3c
redsheep <codingsouls@gmail.com>
2016-07-17 12:49:32 +0800

    update examples

--- component/label.js
+++ component/label.js
@@ -6,17 +6,13 @@ var lblCanvas = new Phaser.Game(480, 150, Phaser.CANVAS, 'lblState',{
     game.stage.backgroundColor='#fff';
     game.gui=new RSGUI(game);
 
-    var label=game.gui.add.label(50,50,'Label Text!');
+    var label=game.gui.add.label(0,0,'Label Text!');
 
-    var label2=game.gui.add.label(50,100,'Label Text!');
+    var label2=game.gui.add.label(0,50,'Label Text!');
     label2.setFont('My Font');
 
-    var label3=game.gui.add.label(50,150,'Label Text!');
+    var label3=game.gui.add.label(0,100,'Label Text!');
     label3.setFont('#ff0000');
 
-    var btnMainState=game.gui.add.button(0,0,'main menu');
-    btnMainState.onClick.add(function(){
-      game.state.start('main');
-    });
   }
 });
--- component/listview.js
+++ component/listview.js
@@ -7,8 +7,13 @@ var lstCanvas = new Phaser.Game(480, 150, Phaser.CANVAS, 'lstState',{
     game.gui=new RSGUI(game);
 
     var button10=game.gui.add.listview(0,0,100,100);
-    var button11=game.gui.add.button(-20,0,"this is a wide button",button10);
-    var button12=game.gui.add.button(-20,40,"this is a wide button",button10);
-
+    /*var button11=game.gui.add.button(-20,0,"this is a wide button");
+    var button12=game.gui.add.button(-20,40,"this is a wide button");
+    var button13=game.gui.add.button(-20,80,"this is a wide button");
+    var button14=game.gui.add.button(-20,120,"this is a wide button");*/
+    button10.addItem("button11");
+    button10.addItem("button12");
+    button10.addItem("button13");
+    button10.addItem("button14");
   }
 });
